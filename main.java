d1d136a0d626460a68d8b39df5380a1b36f10c81
deepakanuraag sathyanarayanan <deepakanuraag@deepakanuraags-MacBook-Air.local>
2020-03-18 18:41:11 -0400

    initial commit

---
+++ main.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class main {
+
+	public static void main(String args[]) {
+
+		String s1 = args[0];
+		String s2 = args[1];
+		
+		HashMap<Character,Character> mp = new HashMap<Character,Character>();
+		
+	}
+}
