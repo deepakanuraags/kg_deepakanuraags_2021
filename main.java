8e5e275facdb2894d4ed5165f918dd3065bf1958
deepakanuraag sathyanarayanan <deepakanuraag@deepakaraagsair.home>
2020-03-19 00:21:48 -0400

    added logic to check if equal length exists and if same character does not map to two different characters

--- main.java
+++ main.java
@@ -9,5 +9,24 @@ public class main {
 		
 		HashMap<Character,Character> mp = new HashMap<Character,Character>();
 		
+		if(s1.length() != s2.length()) {
+			System.out.println("false");
+			return;
+		}
+		for(int i=0;i<s1.length();i++) {
+			
+			if(mp.get(s1.charAt(i))== null) {
+				// if not found put it in map
+				mp.put(s1.charAt(i),s2.charAt(i));
+			}else {
+				// if the a character is found to have a different mapping , its false, since foo cannot map to bar, but bar can map to foo
+				char c = mp.get(s1.charAt(i));
+				if(c != s2.charAt(i)) {
+					System.out.println("false");
+					return;
+				}
+			}
+		}
+		System.out.println("true");
 	}
 }
